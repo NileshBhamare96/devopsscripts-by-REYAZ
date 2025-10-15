76bf60d6e8286dd2e9b50a5b75ac199c0ac80be3
NileshBhamare96 <128180068+NileshBhamare96@users.noreply.github.com>
2025-10-16 04:16:31 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,12 +1,20 @@
-# Step 1: Install Java 17 (Amazon Corretto)
-sudo yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.10/bin/apache-tomcat-11.0.10.tar.gz
-tar -zxvf apache-tomcat-11.0.10.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-11.0.10/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-11.0.10/webapps/manager/META-INF/context.xml
-sh apache-tomcat-11.0.10/bin/startup.sh
+# Switch to home directory
+cd /home/ec2-user
+
+# Step 0: Clean previous Tomcat (if exists)
+rm -rf apache-tomcat-9.* *.tar.gz
+
+# Step 1: Install Java 17 (Corretto)
+yum install java-17-amazon-corretto -y
+
+dnf install java-17-amazon-corretto -y
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.111/bin/apache-tomcat-9.0.111.tar.gz
+tar -zxvf apache-tomcat-9.0.111.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.111/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-9.0.111/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.111/bin/startup.sh
